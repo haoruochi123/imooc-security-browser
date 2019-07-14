726cad7e3c6445b79ef55eaa734b07da753e55a9
haoruochi123 <1092129581@qq.com>
2019-07-14 17:52:27 +0800

    短信登录

--- src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
+++ src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
@@ -4,6 +4,8 @@ import com.imooc.security.browser.handler.HrcAthenticationFailureHandler;
 import com.imooc.security.browser.handler.HrcAuthenticationSuccessHandler;
 import com.imooc.security.core.properties.SecurityProperties;
 import com.imooc.security.core.validate.code.ValidateCodeFilter;
+import com.imooc.security.core.validate.sms.SmsCodeAuthenticationSecurityConfig;
+import com.imooc.security.core.validate.sms.SmsCodeFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,76 +25,86 @@ import org.springframework.security.web.authentication.rememberme.PersistentToke
 import javax.sql.DataSource;
 
 /**
+ * @author 郝若池
  * @ClassName: BrowserSecurityConfig
  * @Description: 权限登录页面配置
- * @author 郝若池
  * @date 2019年6月2日 下午4:58:44
  */
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	private static final Logger logger = LoggerFactory.getLogger(BrowserSecurityConfig.class);
+    private static final Logger logger = LoggerFactory.getLogger(BrowserSecurityConfig.class);
 
-	@Autowired
-	private SecurityProperties securityProperties;
+    @Autowired
+    private SecurityProperties securityProperties;
 
-	@Autowired
-	private HrcAuthenticationSuccessHandler hrcAuthenticationSuccessHandler;
+    @Autowired
+    private HrcAuthenticationSuccessHandler hrcAuthenticationSuccessHandler;
 
-	@Autowired
-	private HrcAthenticationFailureHandler hrcAthenticationFailureHandler;
+    @Autowired
+    private HrcAthenticationFailureHandler hrcAthenticationFailureHandler;
 
-	@Autowired
-	private DataSource dataSource;
+    @Autowired
+    private DataSource dataSource;
 
-	@Autowired
-	private UserDetailsService userDetailsService;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
-	@Bean
-	public RedirectStrategy redirectStrategy() {
-		return new DefaultRedirectStrategy();
-	}
+    @Autowired
+    private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
 
-	// 用户密码加密方法
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    public RedirectStrategy redirectStrategy() {
+        return new DefaultRedirectStrategy();
+    }
 
-	// 记住我的token
-	@Bean
-	public PersistentTokenRepository persistentTokenRepository() {
-		JdbcTokenRepositoryImpl jdbcTokenRepositoryImpl = new JdbcTokenRepositoryImpl();
-		jdbcTokenRepositoryImpl.setDataSource(dataSource);
-		//第一次建表后注释掉，不然要报错
+    // 用户密码加密方法
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // 记住我的token
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepositoryImpl = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepositoryImpl.setDataSource(dataSource);
+        //第一次建表后注释掉，不然要报错
 //		jdbcTokenRepositoryImpl.setCreateTableOnStartup(true);
-		return jdbcTokenRepositoryImpl;
-	}
+        return jdbcTokenRepositoryImpl;
+    }
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		// 图形验证器
-		ValidateCodeFilter validateCodeFilter = new ValidateCodeFilter();
-		validateCodeFilter.setAuthenticationFailureHandler(hrcAthenticationFailureHandler);
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 图形验证器
+        ValidateCodeFilter validateCodeFilter = new ValidateCodeFilter();
+        validateCodeFilter.setAuthenticationFailureHandler(hrcAthenticationFailureHandler);
 
-		http.addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class).formLogin()
-				// .loginPage("/autentication/require")
-				.loginPage(securityProperties.getBrowser().getLoginPage()).loginProcessingUrl("/authentication/form")
-				.successHandler(hrcAuthenticationSuccessHandler).failureHandler(hrcAthenticationFailureHandler).and()
-				.rememberMe().tokenRepository(persistentTokenRepository())
-				.tokenValiditySeconds(securityProperties.getBrowser().getTokenValiditySeconds())
-				.userDetailsService(userDetailsService).and().authorizeRequests()
-				.antMatchers("/login/**", "/authentication/form", "/autentication/require", "/code/image","/exception","/post","/user",
-						securityProperties.getBrowser().getLoginPage())
-				.permitAll().anyRequest().authenticated().and().csrf().disable();
-		logger.info("security配置完成");
-	}
-	/*
-	 * @Override protected void configure(AuthenticationManagerBuilder auth) throws
-	 * Exception { auth.inMemoryAuthentication()
-	 * .withUser("admin").password("admin").roles("ADMIN") .and()
-	 * .withUser("terry").password("terry").roles("USER") .and()
-	 * .withUser("larry").password("larry").roles("USER"); }
-	 */
+        //短信验证过滤器
+        SmsCodeFilter smsCodeFilter = new SmsCodeFilter();
+        smsCodeFilter.setAuthenticationFailureHandler(hrcAthenticationFailureHandler);
+
+        http
+                .addFilterBefore(smsCodeFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class).formLogin()
+                // .loginPage("/autentication/require")
+                .loginPage(securityProperties.getBrowser().getLoginPage()).loginProcessingUrl("/authentication/form")
+                .successHandler(hrcAuthenticationSuccessHandler).failureHandler(hrcAthenticationFailureHandler).and()
+                .rememberMe().tokenRepository(persistentTokenRepository())
+                .tokenValiditySeconds(securityProperties.getBrowser().getTokenValiditySeconds())
+                .userDetailsService(userDetailsService).and().authorizeRequests()
+                .antMatchers("/login/**", "/authentication/form", "/autentication/require", "/code/**", "/exception", "/post", "/user",
+                        securityProperties.getBrowser().getLoginPage())
+                .permitAll().anyRequest().authenticated().and().csrf().disable()
+                .apply(smsCodeAuthenticationSecurityConfig);
+        logger.info("security配置完成");
+    }
+    /*
+     * @Override protected void configure(AuthenticationManagerBuilder auth) throws
+     * Exception { auth.inMemoryAuthentication()
+     * .withUser("admin").password("admin").roles("ADMIN") .and()
+     * .withUser("terry").password("terry").roles("USER") .and()
+     * .withUser("larry").password("larry").roles("USER"); }
+     */
 
 }
